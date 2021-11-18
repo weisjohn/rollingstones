ba4903d5ab8965d52aff8e78a6b99dc9b14e4542
John Weis <weis.john@gmail.com>
2021-11-17 20:36:51 -0500

    src: fix off-by-one in title format

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function generateMilestones() {
     const milestone_start = dayjs(start).add(i * sprint_length_weeks, "weeks");
     return {
       id: group_id,
-      title: `22-${i.toString().padStart(2, '0')}`,
+      title: `22-${(i+1).toString().padStart(2, '0')}`,
       start_date: milestone_start.format("YYYY-MM-DD"),
       due_date: milestone_start.add(sprint_days, "days").format("YYYY-MM-DD"),
     };
